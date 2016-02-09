2e3e4ae9a217d72e0c0384d9216c2c2b88602d0d
ceradon <ceradon_wiki@hotmail.com>
2016-02-09 15:12:05 -0500

    Start

--- script.py
+++ script.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python
 
+class Letter_Game(object):
+    def __init__(self):
+        self._intro = [
+            ""
+        ]
+        
+        self._questions = [
+            ("What does AMS stand for?", "Administrative Management Society"),
+            ("In a block style letter, how many times do you press enter after the date?",
+                "4", "4 times", "four"),
+            ("A letter has its date indented. What style does it use?", "modified block style"),
+            ("If 'cc' is on a letter, what does it stand for?", "carbon copy")
+        ]
